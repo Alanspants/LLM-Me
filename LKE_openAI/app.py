9146cad509f7cca91fca9c4c3bf68d01a7cf7754
alannchen <alannchen@tencent.com>
2025-02-16 11:36:14 +0800

    20250216 home

--- LKE_openAI/app.py
+++ LKE_openAI/app.py
@@ -1,9 +1,12 @@
+import csv
+import json
 import os.path
 
-from flask import Flask, request, render_template, session, redirect, jsonify
+from flask import Flask, request, render_template, session, redirect, jsonify, url_for
 
 app = Flask(__name__)
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
 
 # <form action="" method="post" id='myForm'>
 #     <button name="button" value="value">Send</button>
@@ -34,15 +37,33 @@ APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 @app.route("/index", methods=['GET', 'POST'])
 def index():
-    print(list(request.files))
-    if len(list(request.files)) != 0:
-        file = request.files['file']
-        file_list = list(request.files.values())
-        file_name = file_list[0].filename
+    if request.method == 'POST':
+        # file upload
+        if list(request.form.keys())[0] == 'file-upload-button' and len(list(request.files)):
 
-        target = os.path.join(APP_ROOT, "datasource/")
-        destination = '/'.join([target, file_name])
-        file.save(destination)
+            file = request.files['file']
+            file_list = list(request.files.values())
+            file_name = file_list[0].filename
+            print("file: " + str(file) + "\nfile_list: " + str(file_list) + "\nfile_name: " + str(file_name))
+
+            target = os.path.join(APP_ROOT, "datasource/")
+            destination = '/'.join([target, file_name])
+            file.save(destination)
+
+            csv_file_path = destination
+            json_file_path = destination[:-len(".csv")] + ".txt"
+            data = {}
+
+            with open(csv_file_path, encoding='utf-8') as csvf:
+                csv_reader = csv.DictReader(csvf)
+                i = 0
+                for rows in csv_reader:
+                    data[i] = rows
+                    i += 1
+            with open(json_file_path, 'w', encoding='utf-8') as jsonf:
+                jsonf.write(json.dumps(data, indent=1, ensure_ascii=False))
+
+            return render_template('index.html', file_name=file_name), 200
 
     return render_template('index.html')
 
